a18c62b7ceba7c32837fd86770f0cf1475e6e2ee
Henry Palmer <henryrpalmer@lewisu.edu>
2021-04-21 23:24:48 -0500

    line_items

--- app.js
+++ app.js
@@ -2,24 +2,43 @@ const express = require('express')
 const ejs = require('ejs')
 const stripe = require('stripe')('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')
 const app = express()
+const bodyParser = require("body-parser")
 
 app.set('view engine', 'ejs')
 app.use(express.static('public'))
+app.use(bodyParser.urlencoded({ extended: false }));
 
-function Item(name, id, price) {
-  this.name = name
-  this.id = id
-  this.price = price
+function Item(name, unit_amount, quantity) {
+  this.price_data = {}
+  this.price_data.currency = 'usd'
+  this.price_data.product_data = {}
+  this.price_data.product_data.name = name
+  this.price_data.unit_amount = unit_amount
+  this.quantity = quantity
 }
 
-item1 = new Item("backBLACK", 123, 24.99)
-item2 = new Item("backBLACK", 123, 24.99)
-item3 = new Item("backBLACK", 123, 24.99)
-
+item1 = new Item("backBLACK",2499, 1)
+item2 = new Item("backMAROON",2499, 1)
+item3 = new Item("maroonIDKMAN",2499, 1)
 const cart = []
 cart.push(item1)
 cart.push(item2)
 cart.push(item3)
+
+
+const line_items = []
+  // {
+  //   price_data: {
+  //     currency: 'usd',
+  //     product_data: {
+  //       name: "T-shirt",
+  //     },
+  //     unit_amount: 2000,
+  //   },
+  //   quantity: 1,
+  // }
+
+
 
 app.get('/', function(req, res) {
     res.render("home")
@@ -66,10 +85,10 @@ app.get("/cancel", function(req, res) {
 })
 
 app.post('/cart', function(req,res) {
-  var sizes = req.body.sizes
-  var qnty = req.body.qnty
-  console.log(sizes, qnty);
-  res.render("cart",{cart: cart})
+  var size = req.body.size
+  var quantity = req.body.qnty
+  // let item = new Item(name, unit_amount, quantity)
+  res.redirect("/cart");
 })
 
 
@@ -80,18 +99,19 @@ app.post('/create-checkout-session', async (req, res) => {
     shipping_address_collection: {
       allowed_countries: ['US'],
     },
-    line_items: [
-      {
-        price_data: {
-          currency: 'usd',
-          product_data: {
-            name: 'T-shirt',
-          },
-          unit_amount: 2000,
-        },
-        quantity: 1,
-      },
-    ],
+    line_items,
+    // line_items: [
+    //   {
+    //     price_data: {
+    //       currency: 'usd',
+    //       product_data: {
+    //         name: 'T-shirt',
+    //       },
+    //       unit_amount: 2000,
+    //     },
+    //     quantity: 1,
+    //   },
+    // ],
     mode: 'payment',
     success_url: 'https://shopvibes.azurewebsites.net/success',
     cancel_url: 'https://shopvibes.azurewebsites.net/cancel',
